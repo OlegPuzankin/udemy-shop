4eab71090dc6ad2e6bdeb3410e86a275fee5b6a8
Oleg Puzankin <oleg.puzankin@gmail.com>
2019-12-28 07:50:57 +0200

    '28-12'

---
+++ src/pages/collection/collection-page.component.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './category-page.styles.scss'
+
+
+export const Category = (props) => {
+    console.log('props from category',props)
+
+    return (
+  <div>
+   Category
+  </div>
+ );
+};
